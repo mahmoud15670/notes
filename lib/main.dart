09fe4f50fc9f4e99d771702873fcc159f84aabcd
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-22 13:16:24 +0000

    Sep 22, 2024, 4:16 PM

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class HomePage extends StatelessWidget {
                 return const VerifyEmailView();
               }
             default:
-              return indecato;
+              return const CircularProgressIndicator();
           }
         },
       );
