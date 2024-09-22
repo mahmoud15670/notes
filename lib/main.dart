f65fa39b3b03105815c73930034cddcce5b6caef
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-22 15:37:05 +0000

    Sep 22, 2024, 6:37 PM

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class HomePage extends StatelessWidget {
               final user = FirebaseAuth.instance.currentUser;
               if (user != null){
                 if (user.emailVerified){
-                  return const Text('done');
+                  return const Scaffold();
                 }else {
                   return const VerifyEmailView();
                 }
