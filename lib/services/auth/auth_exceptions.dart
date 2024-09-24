5e26fceb7be56362f2c98651ed9ef495a01eaa0e
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-24 22:58:20 +0000

    Sep 25, 2024, 1:58 AM

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,4 @@
+class UserNotFountAuthException implements Exception{}
+class WrongPasswordAuthException implements Exception{}
+class WeakPasswordAuthException implements Exception{}
+class UserNotFountAuthException implements Exception{}
