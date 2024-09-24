eac1bed8fd2b75aa77a765125b0e06c92c04014d
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-24 23:52:43 +0000

    Sep 25, 2024, 2:52 AM

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -1,3 +1,38 @@
-import 'auth_exceptions.dart';
-import 'auth_provider.dart';
-import 'auth_user.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/auth_user.dart';
+
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
+
+class FirebaseAuthProvider implements AuthProvider{
+  @override
+  Future<AuthUser> createUser({required String email, required String password}) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  AuthUser? get currentUser {
+    
+  }
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+
+}
