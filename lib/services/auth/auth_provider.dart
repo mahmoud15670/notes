0be680422f07daac1ad3e6bf11db2c357d3efb81
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-24 23:40:50 +0000

    Sep 25, 2024, 2:40 AM

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -6,4 +6,8 @@ abstract class AuthProvider {
     required String email,
     required String password,
   });
+  Future<AuthUser> createUser ({
+    required String email,
+    required String password,
+  });
 }
