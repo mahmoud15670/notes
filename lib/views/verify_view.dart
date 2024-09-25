a4ce1e49226236161c63f501462279ec634d2af8
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-25 12:37:17 +0000

    Sep 25, 2024, 3:37 PM

--- lib/views/verify_view.dart
+++ lib/views/verify_view.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constance/routs.dart';
+import 'package:mynotes/services/auth/auth_services.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -13,34 +13,36 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Email verfiy'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Column(
-        children: [
-          const Text("we've send a veritation email please check your inbox."),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text("if you didn't reseved email "),
-              TextButton(
+        appBar: AppBar(
+          title: const Text('Email verfiy'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: Column(
+          children: [
+            const Text(
+                "we've send a veritation email please check your inbox."),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("if you didn't reseved email "),
+                TextButton(
+                  onPressed: () async {
+                    await AuthServices.firebase().sendEmailVerification();
+                  },
+                  child: const Text('send email vertfiction'),
+                ),
+              ],
+            ),
+            TextButton(
                 onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                await user?.sendEmailVerification();
-                }, 
-                child: const Text('send email vertfiction'),
-              ),
-            ],
-          ),
-          TextButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.of(context).pushNamedAndRemoveUntil(registerRout, (route) => false,);
-            }, 
-            child: const Text('back'))
-        ],
-      )
-    );
+                  await AuthServices.firebase().logOut();
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    registerRout,
+                    (route) => false,
+                  );
+                },
+                child: const Text('back'))
+          ],
+        ));
   }
 }
