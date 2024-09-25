2afaecf96aed616d5accc0b7ebd9c4b82113fe86
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-25 12:35:58 +0000

    Sep 25, 2024, 3:35 PM

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constance/routs.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_services.dart';
 import 'package:mynotes/utilities/error_dialog.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
@@ -32,67 +34,68 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Column(
-      children: [
-        TextField(
-          controller: _email,
-          enableSuggestions: false,
-          autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration:
-              const InputDecoration(hintText: 'Enter your email'),
+        appBar: AppBar(
+          title: const Text('Register'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
-        TextField(
-          controller: _password,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration:
-              const InputDecoration(hintText: 'Enter your password'),
-        ),
-        TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
-              await FirebaseAuth.instance
-                  .createUserWithEmailAndPassword(
-                      email: email, password: password);
-              final user = FirebaseAuth.instance.currentUser;
-              user?.sendEmailVerification();
-              Navigator.of(context).pushNamed(verfiyRout);
-              }on FirebaseAuthException catch (e) {
-                if (e.code == 'email-already-in-use'){
-                  await showErrorDialog(context, 'The email address is already in use by another account.');
-                }else if (e.code == 'weak-password'){
-                  await showErrorDialog(context, 'The password is weak');
-                }else if (e.code == 'invalid-email'){
-                  await showErrorDialog(context, 'invalid email');
-                }else{
-                  await showErrorDialog(context, '${e.message}');
-                }
-              }catch (e) {
-                await showErrorDialog(context, e.toString());
-              }
-            },
-            child: const Text('Register')),
+        body: Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(hintText: 'Enter your email'),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your password'),
+            ),
+            TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+                  try {
+                    await AuthServices.firebase().createUser(
+                      email: email,
+                      password: password,
+                    );
+                    await AuthServices.firebase().sendEmailVerification();
+                    Navigator.of(context).pushNamed(verfiyRout);
+                  } on EmailAlradyInUseAuthException {
+                    await showErrorDialog(
+                      context,
+                      'The email address is already in use by another account.',
+                    );
+                  } on WeakPasswordAuthException {
+                    await showErrorDialog(context, 'The password is weak');
+                  } on InvaidEmailAuthException {
+                    await showErrorDialog(context, 'invalid email');
+                  } on GenericAuthException {
+                    await showErrorDialog(context,
+                        'an error happend check network or your certional');
+                  }
+                },
+                child: const Text('Register')),
             Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("have account ?"),
-                  TextButton(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("have account ?"),
+                TextButton(
                     onPressed: () {
-                      Navigator.of(context).pushNamedAndRemoveUntil(loginRout, (route) => false,);
-                    }, 
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        loginRout,
+                        (route) => false,
+                      );
+                    },
                     child: const Text('login !'))
-                ],
-              )
-      ],
-    )
-  );
+              ],
+            )
+          ],
+        ));
   }
 }
