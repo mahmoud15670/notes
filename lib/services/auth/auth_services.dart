e9141ee0f4010d3ce8b8af67378e1335c9f102e7
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-25 12:08:51 +0000

    Sep 25, 2024, 3:08 PM

--- lib/services/auth/auth_services.dart
+++ lib/services/auth/auth_services.dart
@@ -35,4 +35,7 @@ class AuthServices implements AuthProvider {
 
   @override
   Future<void> sendEmailVerification() => provider.sendEmailVerification();
+
+  @override
+  Future<void> initialize() => provider.initialize();
 }
