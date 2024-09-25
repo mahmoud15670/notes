6d579116d7fdc080d078465f1d08a6a0e2396450
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-25 12:18:04 +0000

    Sep 25, 2024, 3:18 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constance/routs.dart';
+import 'package:mynotes/services/auth/auth_services.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_view.dart';
-import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,15 +31,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
+        future: AuthServices.firebase().initialize(),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
+              final user = AuthServices.firebase().currentUser;
               if (user != null){
-                if (user.emailVerified){
+                if (user.isEmailVerified){
                   return const NotesView();
                 }else {
                   return const VerifyEmailView();
