250d36f0bd8c091f02725a4cda094c75de246425
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-26 11:10:12 +0000

    Sep 26, 2024, 2:10 PM

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,4 +1,42 @@
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/auth_user.dart';
 import 'package:test/test.dart';
 void main () {}
 
-class MoukAuthProvider implements au
+class MoukAuthProvider implements AuthProvider {
+  final <AuthUser;> user;
+  @override
+  Future<AuthUser> createUser({required String email, required String password}) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> initialize() {
+    // TODO: implement initialize
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+
+}
