f0b9c2976e1cb443cc753a23c21e431344feba05
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-26 21:55:07 +0000

    Sep 27, 2024, 12:55 AM

---
+++ lib/services/crud/notes_services.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' show join;
+
+@immutable
+class DataBaseUser {
+  final int id;
+  final String email;
+
+  const DataBaseUser({
+    required this.id,
+    required this.email,
+  });
+
+  DataBaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  String toString() {
+    return 'userId = $id and his email = $email';
+  }
+}
+
+const idColumn = 'id';
+const emailColumn = 'email';
