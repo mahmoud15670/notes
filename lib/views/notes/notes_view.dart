fb09982988088360f30df8a14cc750fbf9b532dd
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-29 21:15:17 +0000

    Sep 30, 2024, 12:15 AM

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -77,6 +77,7 @@ class _NotesViewState extends State<NotesView> {
                   builder: (context, snapshot) {
                     switch (snapshot.connectionState) {
                       case ConnectionState.waiting:
+                      case ConnectionState.active:
                         return const Text('data');
                       default:
                         return const CircularProgressIndicator();
