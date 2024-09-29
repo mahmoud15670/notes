9da18e023a0fb243b5e2e91bfd5c9a21ffd63df3
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-29 04:58:12 +0000

    Sep 29, 2024, 7:58 AM

---
+++ lib/views/notes/add_new_note.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNewNote extends StatefulWidget {
+  const AddNewNote({super.key});
+
+  @override
+  State<AddNewNote> createState() => _AddNewNoteState();
+}
+
+class _AddNewNoteState extends State<AddNewNote> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
