9583327ee47ff4386bb6220593c5b7032505b43e
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-29 21:19:16 +0000

    Sep 30, 2024, 12:19 AM

--- lib/views/notes/add_new_note.dart
+++ lib/views/notes/add_new_note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mynotes/services/crud/notes_services.dart';
 
 class AddNewNote extends StatefulWidget {
   const AddNewNote({super.key});
@@ -6,8 +7,9 @@ class AddNewNote extends StatefulWidget {
   @override
   State<AddNewNote> createState() => _AddNewNoteState();
 }
-
 class _AddNewNoteState extends State<AddNewNote> {
+  DataBaseNote? _note;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
