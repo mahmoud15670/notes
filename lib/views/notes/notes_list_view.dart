cb47fb41c13c7585cd8e199243d2c1011a057705
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-30 21:46:49 +0000

    Oct 1, 2024, 12:46 AM

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -2,9 +2,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/crud/notes_services.dart';
 
+typedef DeleteNoteCallback = void Function(DataBaseNote note);
+
 class NotesListView extends StatelessWidget {
   final List<DataBaseNote> notes;
-  
+  final delete
   const NotesListView({
     super.key,
     required this.notes,
