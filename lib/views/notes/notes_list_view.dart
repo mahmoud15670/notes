e862d617427691dadb1e017c8e95cd15297ea7f7
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-30 21:40:57 +0000

    Oct 1, 2024, 12:40 AM

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/services/crud/notes_services.dart';
 
 class NotesListView extends StatelessWidget {
-  const NotesListView({super.key});
+  final List<DataBaseNote> notes;
+  const NotesListView({
+    super.key,
+    required this.notes,
+  });
 
   @override
   Widget build(BuildContext context) {
