4f028657dc810b51ae2cb10b9a2291548f2a5961
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-01 19:59:53 +0000

    Oct 1, 2024, 10:59 PM

--- lib/utilities/generics/get_arguments.dart
+++ lib/utilities/generics/get_arguments.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart'show BuildContext, ModalRoute;
+import 'package:flutter/material.dart' show BuildContext, ModalRoute;
 
 extension GetArguments on BuildContext {
-  T? getArgument<T>(){
-  final modalRoute = ModalRoute.of(this);
-  if (modalRoute != null){
-    final args = modalRoute.settings.arguments;
-    if(args != null && ){
-      return args as T;
+  T? getArgument<T>() {
+    final modalRoute = ModalRoute.of(this);
+    if (modalRoute != null) {
+      final args = modalRoute.settings.arguments;
+      if (args != null && args is T) {
+        return args as T;
+      }
     }
-  }
-  return null;
+    return null;
   }
 }
