794f45a0f9a044253bc36852721b51c931d51b5f
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-01 20:37:08 +0000

    Oct 1, 2024, 11:37 PM

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -81,6 +81,12 @@ class _NotesViewState extends State<NotesView> {
                             onDeleteNote: (note) async {
                               await _notesServices.deleteNote(id: note.id);
                             },
+                            onTapNote: (note) {
+                              Navigator.of(context).pushNamed(
+                                createOrUpdateNoteRout,
+                                arguments: note
+                              );
+                            },
                           );
                         } else {
                           return const CircularProgressIndicator();
