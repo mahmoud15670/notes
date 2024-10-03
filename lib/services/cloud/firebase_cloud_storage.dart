a9a35e956dca861ccdeedab02e4dca0e1677e14f
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-03 22:20:44 +0000

    Oct 4, 2024, 1:20 AM

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,11 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
+import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
 
   void createNewNote({required String ownerUserId})async {
-    await notes.add();
+    await notes.add({
+        ownerUserIdField:ownerUserId,
+        textField:''
+    });
   }
 
   static final FirebaseCloudStorage _shared =
