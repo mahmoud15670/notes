105a0037bff29e92dfdf5ecd9122557409f530bf
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-03 19:21:14 +0000

    Oct 3, 2024, 10:21 PM

---
+++ lib/services/cloud/cloud_note.dart
@@ -0,0 +1,11 @@
+class CloudNote {
+  final String documentId;
+  final String userId;
+  final String text;
+
+  const CloudNote({
+    required this.documentId,
+    required this.userId,
+    required this.text,
+  });
+}
