f5a5dc00777da06db7ab27ac97f7b3bbb0d66d87
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-03 22:09:27 +0000

    Oct 4, 2024, 1:09 AM

---
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -0,0 +1,6 @@
+class FirebaseCloudStorage {
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
+  FirebaseCloudStorage._sharedInstance();
+  factory FirebaseCloudStorage() => _shared;
+}
