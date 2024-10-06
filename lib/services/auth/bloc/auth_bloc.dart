bee76b0d34e3a14ffefaf5015179006fe5962db7
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-06 17:17:33 +0000

    Oct 6, 2024, 8:17 PM

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
+import 'package:mynotes/services/auth/firebase_auth_provider.dart';
 
 @immutable
-abstract class AuthBloc {
-  final AuthProvider provider;
-
-  const AuthBloc(this.provider);
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()){
+    
+  }
 }
