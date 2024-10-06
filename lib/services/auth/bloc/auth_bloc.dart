00680c726c1d6a61a65272b28711cef7a9fadd3c
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-06 20:53:22 +0000

    Oct 6, 2024, 11:53 PM

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -1,13 +1,40 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/auth/bloc/auth_state.dart';
-import 'package:mynotes/services/auth/firebase_auth_provider.dart';
 
-@immutable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()){
-    
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    on<AuthEventInitialize>((event, emit) async {
+      await provider.initialize();
+      final user = provider.currentUser;
+      if (user == null) {
+        emit(const AuthStateLoggedOut());
+      } else if (!user.isEmailVerified) {
+        emit(const AuthStateNeedsVerification());
+      } else {
+        AuthStateLoggedIn(user);
+      }
+    });
+    on<AuthEventLogin>((event, emit) async {
+      emit(const AuthStateLoading());
+      try {
+        final user = await provider.logIn(
+          email: event.email,
+          password: event.password,
+        );
+        emit(AuthStateLoggedIn(user));
+      } on Exception catch (e) {
+        emit(AuthStateLoggedInFalier(e));
+      }
+    });
+    on<AuthEventLogout>((event, emit) async {
+      try {
+        await provider.logOut();
+        emit(AuthStateLoggedOut());
+      } on Exception catch (e) {
+        emit(AuthStateLoggedOutFalier(e));
+      }
+    });
   }
 }
