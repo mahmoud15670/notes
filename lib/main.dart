ac8d7e159815089e7aa8c86b147d173ac7bcc66c
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-06 21:52:41 +0000

    Oct 7, 2024, 12:52 AM

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class HomePage extends StatelessWidget {
     context.read<AuthBloc>().add(const AuthEventInitialize());
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
+        print(state);
         if (state is AuthStateLoggedIn) {
           return const NotesView();
         } else if (state is AuthStateNeedsVerification) {
