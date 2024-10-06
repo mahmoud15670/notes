2006312d1c09c5c7248088bb456ef9ee6462d8bb
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-06 16:47:01 +0000

    Oct 6, 2024, 7:47 PM

--- lib/services/auth/bloc/auth_bloc_event.dart
+++ lib/services/auth/bloc/auth_bloc_event.dart
@@ -4,3 +4,19 @@ import 'package:flutter/foundation.dart';
 abstract class AuthBlocEvent {
   const AuthBlocEvent();
 }
+
+class AuthEventLoading extends AuthBlocEvent {
+  const AuthEventLoading();
+}
+
+class AuthEventLogin extends AuthBlocEvent {
+  const AuthEventLogin();
+}
+
+class AuthEventverfitemail extends AuthBlocEvent {
+  const AuthEventverfitemail();
+}
+
+class AuthEventLogout extends AuthBlocEvent {
+  const AuthEventLogout();
+}
