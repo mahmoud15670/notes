84cce32ed89e182648335bfe4cb601141f5496b6
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-06 16:36:26 +0000

    Oct 6, 2024, 7:36 PM

---
+++ lib/services/auth/bloc/auth_bloc_state.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+abstract class AuthBlocState {
+  const AuthBlocState();
+}
+
+class AuthBlocInitialze extends AuthBlocState {
+  const AuthBlocInitialze();
+}
+
+class AuthBlocLoggedIn extends AuthBlocState {
+  const AuthBlocLoggedIn();
+}
