fd73fb10a8cdab5d8b11ac051c32881c3afd4e18
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-09 15:44:29 +0000

    Oct 9, 2024, 6:44 PM

---
+++ lib/helpers/loading/loading_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mynotes/helpers/loading/loading_screen_controller.dart';
+
+class LoadingScreen {
+  factory LoadingScreen() => _shared;
+  static final _shared = LoadingScreen._sharedInstance();
+  LoadingScreen._sharedInstance();
+
+  LoadingScreenController? controller;
+
+  LoadingScreenController showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final _text = StreamController<String>();
+    _text.add(text);
+    final state = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+    final overlay = OverlayEntry(builder: (context) {
+      return Material();
+    });
+    state.insert(overlay);
+    return LoadingScreenController(
+      close: () {
+        _text.close();
+        overlay.remove();
+        return true;
+      },
+      update: (text) {
+        _text.add(text);
+        return true;
+      },
+    );
+  }
+}
