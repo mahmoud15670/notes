1cdeebf7acf2c5e5cba25d89807a8a6a5685117f
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-10-09 17:32:10 +0000

    Oct 9, 2024, 8:32 PM

--- lib/helpers/loading/loading_screen.dart
+++ lib/helpers/loading/loading_screen.dart
@@ -44,8 +44,8 @@ class LoadingScreen {
           child: Container(
             constraints: BoxConstraints(
               maxHeight: size.height * 0.8,
-              maxWidth: size.width*0.8,
-              minWidth: size.width*0.5,
+              maxWidth: size.width * 0.8,
+              minWidth: size.width * 0.5,
             ),
             decoration: BoxDecoration(
               color: Colors.white,
@@ -55,7 +55,29 @@ class LoadingScreen {
               padding: const EdgeInsets.all(16.0),
               child: SingleChildScrollView(
                 child: Column(
-                  
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const CircularProgressIndicator(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StreamBuilder(
+                      stream: _text.stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.data as String,
+                            textAlign: TextAlign.center,
+                          );
+                        }
+                        return Container();
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
